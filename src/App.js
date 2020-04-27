8c0d1c165f951ab90acd54ccea2560a051332193
Carlos Zuniga <carlosrozuma@gmail.com>
2020-04-26 18:17:28 -0700

    componente Formulario creado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Fragment } from 'react';
+import Formulario from './components/Formulario';
 
 function App() {
   return (
     <Fragment>
       <h1>Administración de pacientes</h1>
       <div className="container">
-        <div className="one-half column"></div>
+        <div className="one-half column">
+          <Formulario />
+        </div>
+
         <div className="one-half column"></div>
       </div>
     </Fragment>
---
+++ src/components/Formulario.jsx
@@ -0,0 +1,9 @@
+import React, { Fragment } from 'react';
+
+const Formulario = () => (
+    <Fragment>
+        <h2>Formulario</h2>
+    </Fragment>  
+);
+ 
+export default Formulario;
